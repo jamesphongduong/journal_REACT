0a7185bf3ab216be9e38c4de2b555b28f8c41619
James Phong Duong <james.duong93@gmail.com>
2019-01-03 23:33:17 +1100

    links added

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,27 @@
 import React, { Component } from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import CategorySelection from "./pages/CategorySelection";
 import NewEntryPage from "./pages/NewEntryPage";
 
 class App extends Component {
+    state = {
+        categories: ["food", "thoughts", "romance"]
+    }
 
     render() {
+        const { categories } = this.state;
+        
         return (
             <div> 
-                {/* BrowserRouter expects a single child, hence we wrap content in div */}
                 <BrowserRouter> 
                     <div>
-                         <Route exact path="/" component={HomePage}/>
-                         <Route exact path="/category" component={CategorySelection}/>
-                         <Route exact path="/entry" component={NewEntryPage}/>
+                        <Link to="/"> Home </Link>
+                        <Link to="/category"> Category </Link>
+                        <Link to="/entry"> Entry </Link>
+                        <Route exact path="/" component = {HomePage} />
+                        <Route exact path="/category" component = {CategorySelection} />
+                        <Route exact path="/entry" component = {NewEntryPage} />
                     </div>
                 </BrowserRouter>
             </div>
