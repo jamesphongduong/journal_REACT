76476790245f603f1073305710d6de3b22198af9
James Phong Duong <james.duong93@gmail.com>
2019-01-03 16:23:28 +1100

    navigation buttons added

--- src/components/App.js
+++ src/components/App.js
@@ -6,12 +6,32 @@ import NewEntryPage from "./pages/NewEntryPage";
 class App extends Component {
     state = { location: "home" }
 
+    getPage() {
+        const { location } = this.state;
+
+        switch(location) {
+            case "home":
+                return <HomePage />
+            case "category":
+                return <CategorySelection />
+            case "entry":
+                return <NewEntryPage />
+            default: 
+                return null;
+        }
+    }
+
+    onChangeLocation = (location) => {
+        this.setState({ location });
+    }
+
     render() {
-        return(
+        return (
             <div> 
-                <HomePage />
-                <CategorySelection />
-                <NewEntryPage />
+                <button onClick={() => this.onChangeLocation("category")}> Category </button>
+                <button onClick={() => this.onChangeLocation("home")}> Home Page </button>
+                <button onClick={() => this.onChangeLocation("entry")}> New Entry Page </button>
+                {this.getPage()}
             </div>
         )
     }
