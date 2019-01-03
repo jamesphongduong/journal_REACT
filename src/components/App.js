e6ecdb6caf0d36d74da692a5a95077028f6ee4fc
James Phong Duong <james.duong93@gmail.com>
2019-01-03 16:52:53 +1100

    routes added

--- src/components/App.js
+++ src/components/App.js
@@ -1,37 +1,22 @@
 import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import CategorySelection from "./pages/CategorySelection";
 import NewEntryPage from "./pages/NewEntryPage";
 
 class App extends Component {
-    state = { location: "home" }
-
-    getPage() {
-        const { location } = this.state;
-
-        switch(location) {
-            case "home":
-                return <HomePage />
-            case "category":
-                return <CategorySelection />
-            case "entry":
-                return <NewEntryPage />
-            default: 
-                return null;
-        }
-    }
-
-    onChangeLocation = (location) => {
-        this.setState({ location });
-    }
 
     render() {
         return (
             <div> 
-                <button onClick={() => this.onChangeLocation("category")}> Category </button>
-                <button onClick={() => this.onChangeLocation("home")}> Home Page </button>
-                <button onClick={() => this.onChangeLocation("entry")}> New Entry Page </button>
-                {this.getPage()}
+                {/* BrowserRouter expects a single child, hence we wrap content in div */}
+                <BrowserRouter> 
+                    <div>
+                         <Route exact path="/" component={HomePage}/>
+                         <Route exact path="/category" component={CategorySelection}/>
+                         <Route exact path="/entry" component={NewEntryPage}/>
+                    </div>
+                </BrowserRouter>
             </div>
         )
     }
