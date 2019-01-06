4f4d53eee3679863b8bd48c1ef8976fb751db380
James Phong Duong <james.duong93@gmail.com>
2019-01-06 18:23:40 +1100

    back button added for edit form page

--- src/components/pages/EditEntryPage.js
+++ src/components/pages/EditEntryPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import EditForm from "./../forms/EditForm";
+import { Link } from "react-router-dom";
 
 class EditEntryPage extends Component {
  
@@ -7,11 +8,13 @@ class EditEntryPage extends Component {
         const { entries, onEditEntryFormSubmit } = this.props;
         const { id } = this.props.match.params;
 
-
         return (
             <div>
                 <h1 className="title-center"> Journal Entry </h1> 
                 <EditForm entries={entries} index={id} onEditEntryFormSubmit={onEditEntryFormSubmit}/>
+            <Link to="/">
+                <button className= "ui button"> Back To Home</button>
+            </Link>
             </div>
         );
     }
